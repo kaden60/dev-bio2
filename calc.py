0fa12f95686f7e462f3e4c493036e89fbef47c4b
Simon <simonk@indianriverschools.org>
2021-02-23 09:14:16 -0500

    updated

---
+++ calc.py
@@ -0,0 +1,12 @@
+
+print( "  ____              ___    ======           ")
+print( " |    \    @       \__ \     ||             ")
+print( " | [] |   ||          \      ||             ")
+print( " L____/   ||      \___/      ||        calc ")
+
+
+a = input("enter the distance: ")
+d = input("enter the time it takes to travel : ")
+
+x = int(a)/int(d)
+print("the object or person is traveling at about " ,x, "units a second")
